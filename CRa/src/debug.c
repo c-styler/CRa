b3b3ea77664252e9e4f8ef3d6265c3fe1b92ace9
som3a-dev <omar.eltayeb54@gmail.com>
2026-08-10 06:31:08 +0300

    fix increment bug in chunk disassembly

--- CRa/src/debug.c
+++ CRa/src/debug.c
@@ -21,7 +21,7 @@ void disassemble_chunk(Chunk* chunk, const char* name)
 {
     printf("== %s ==\n", name);
 
-    for (int offset = 0; offset < chunk->count; offset++)
+    for (int offset = 0; offset < chunk->count;)
     {
         offset = disassemble_instruction(chunk, offset);
     }
