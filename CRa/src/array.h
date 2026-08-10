6ae95fcac40b420365563e03e41eeb75d5e09385
som3a-dev <omar.eltayeb54@gmail.com>
2026-08-10 04:57:31 +0300

    rename project

---
+++ CRa/src/array.h
@@ -0,0 +1,13 @@
+/*
+ * array.h
+ * Created on Mon Aug 10 2026
+ * Part of C-Ra — SPDX-License-Identifier: MIT
+ * Copyright (c) 2026 Omar Eltayeb
+ *
+ * Shared code for dynamic array implementations
+ */
+
+#ifndef ra_array_h
+#define ra_array_h
+
+#endif
---
+++ CRa/src/value.h
@@ -0,0 +1,15 @@
+/*
+ * memory.c
+ * Created on Mon Aug 10 2026
+ * Part of C-Ra — SPDX-License-Identifier: MIT
+ * Copyright (c) 2026 Omar Eltayeb
+ *
+ * Defines the representation of Ra values
+ */
+
+#ifndef ra_value_h
+#define ra_value_h
+
+typedef double Value;
+
+#endif
