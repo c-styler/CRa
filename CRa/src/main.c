3c683d2ce08c747796dcf70452eddcb943ac91f1
som3a-dev <omar.eltayeb54@gmail.com>
2026-08-17 20:32:36 +0300

    repl and run_file, scanner test

--- CRa/src/main.c
+++ CRa/src/main.c
@@ -8,37 +8,100 @@
 #include "code.h"
 #include "common.h"
 #include "debug.h"
+#include "scanner.h"
 #include "vm.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static void repl();
+static void run_file(const char* path);
+
+static VM vm = {0};
 
 int main(int argc, char* argv[])
 {
-    Chunk chunk;
-    chunk_init(&chunk);
+    if (argc == 1)
+    {
+    }
+    else if (argc == 2)
+    {
+    }
 
-    int constant_index = chunk_push_constant(&chunk, 1.2);
-    chunk_push(&chunk, OP_CONSTANT, 1);
-    chunk_push(&chunk, constant_index, 1);
+    Scanner scanner = {0};
+    scanner_init(&scanner, "1 1.0 3 14.2\n256 * 128");
+    while (true)
+    {
+        Token token = scanner_scan_token(&scanner);
+        if (token.type == TOKEN_EOF)
+        {
+            break;
+        }
 
-    constant_index = chunk_push_constant(&chunk, 1.2);
-    chunk_push(&chunk, OP_CONSTANT, 123);
-    chunk_push(&chunk, constant_index, 123);
+        if (token.type == TOKEN_ERROR)
+        {
+            printf("Syntax Error: %s\n", token.start);
+            break;
+        }
 
-    chunk_push(&chunk, OP_ADD, 123);
-
-    constant_index = chunk_push_constant(&chunk, 5.6);
-    chunk_push(&chunk, OP_CONSTANT, 123);
-    chunk_push(&chunk, constant_index, 123);
-
-    chunk_push(&chunk, OP_DIVIDE, 123);
-    chunk_push(&chunk, OP_NEGATE, 123);
-    chunk_push(&chunk, OP_RETURN, 2);
-
-    VM vm = {0};
-    vm_init(&vm);
-    vm_interpret(&vm, &chunk);
-
-    //    disassemble_chunk(&chunk, "Chunk1");
-    chunk_clear(&chunk);
+        for (int i = 0; i < token.length; i++)
+        {
+            printf("%c", token.start[i]);
+        }
+        printf("\n");
+    }
 
     return 0;
+}
+
+static void repl()
+{
+    char line[256];
+
+    while (true)
+    {
+        printf("> ");
+        fgets(line, sizeof(line), stdin);
+
+        line[strlen(line) - 1] = '\0'; // strip the \n
+
+        if (strcmp(line, ".q") == 0)
+        {
+            break;
+        }
+
+        vm_interpret(&vm, line);
+    }
+}
+
+static void run_file(const char* path)
+{
+    FILE* fp = fopen(path, "rb");
+    if (!fp)
+    {
+        fprintf(stderr, "Could not open file \"%s\".\n", path);
+        return;
+    }
+
+    fseek(fp, 0L, SEEK_END);
+    size_t file_size = ftell(fp);
+    rewind(fp);
+
+    char* file_string = malloc(sizeof(char) * (file_size + 1));
+    if (file_string == NULL)
+    {
+        fprintf(stderr, "Not enough memory to read \"%s\"\n", path);
+        return;
+    }
+
+    size_t characters_read =
+        fread(file_string, sizeof(char), file_size, fp);
+    file_string[characters_read] = '\0';
+
+    fclose(fp);
+
+    vm_interpret(&vm, file_string);
+
+    free(file_string);
 }
